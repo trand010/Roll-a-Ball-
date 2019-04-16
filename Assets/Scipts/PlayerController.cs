dd669534971695d33f1cc33082bf3cb19636f260
Tran_Daniel <trand010@hsd.k12.or.us>
2019-04-16 09:56:33 -0700

    respawn

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -28,6 +28,11 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+
+        if (rb.position.y < -12)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -43,9 +48,10 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 32)
+        if (count >= 10)
         {
             winText.text = "You Win!";
+
         }
     }
 }
